b7afa0a2e5a6bada860fd0e8af768777a91bf1da
nicolasseng <116186244+nicolasseng@users.noreply.github.com>
2023-05-05 11:31:54 +0200

    Added show the code

--- FirstInterface.py
+++ FirstInterface.py
@@ -1,15 +1,10 @@
-import glob
-import os
-import time
 import urllib
 from io import StringIO
 
 import altair as alt
-import cv2
 import numpy as np
 import pandas as pd
 import streamlit as st
-import wget
 from PIL import Image
 
 st.set_page_config(page_title="Object Detection Web App", layout="wide")
@@ -23,11 +18,7 @@ st.write("Open the Sidebar to either upload a file or show the source code.")
 
 
 def main():
-    # readme_text = st.markdown(
-    #     get_file_content_as_string(
-    #         "README.md?token=GHSAT0AAAAAACBLTP6HFVNQEJ4ANB3J7ZB2ZCSKYNQ"
-    #     )
-    # )
+    readme_text = st.markdown(get_file_content_as_string("README.md"))
     st.sidebar.title("What to do?")
     app_mode = st.sidebar.selectbox(
         "What do you want to do?",
@@ -36,13 +27,8 @@ def main():
     if app_mode == "Run the App":
         run_the_app()
     elif app_mode == "Show the Code":
-        # readme_text.empty()
-        # st.code(
-        #    get_file_content_as_string(
-        #        "test.py?token=GHSAT0AAAAAACBLTP6HI5ERK5IEFXEY4PN4ZCSKVXA"
-        #    )
-        # )
-        st.write("Working on it...")
+        readme_text.empty()
+        st.code(get_file_content_as_string("FirstInterface.py"))
     elif app_mode == "Upload a File":
         upload_file()
 
@@ -77,24 +63,20 @@ def get_file_content_as_string(path):
     return response.read().decode("utf-8")
 
 
-def infer_image(img, size=None):
-    model.conf = confidence
-    result = model(img, size=size) if size else model(img)
-    result.render()
-    image = Image.fromarray(result.ims[0])
-    return image
-
-
 def run_the_app():
     st.sidebar.markdown("# Settings")
-
     model_src = st.sidebar.radio(
         "Select file", ["Use our demo files", "Use your own files"]
     )
     if model_src == "Use your own files":
         upload_file()
+    elif model_src == "Use our demo files":
+        image = Image.open("data/sample_img/Test_Image.png")
+        st.image(image, caption="This is an example image")
 
-    input_option = st.sidebar.radio("Select input type: ", ["image", "video"])
+    input_option = st.sidebar.radio(
+        "Select input type: ", ["image", "video", "livestream"]
+    )
 
     model_names = list({"bikes", "car", "Traffic light", "human"})
     if st.sidebar.checkbox("Search for specific objects?"):
